648a1359b44afe2ca01d0358c14bfbfa5c25929d
tundeUsman <tundeusman0@gmail.com>
2019-09-12 14:50:37 +0100

    added resume post/get/update api

---
+++ backend/routes/api/resume.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const Resume = require('../../models/resume');
+const auth = require('../../middleware/auth');
+
+const router = new express.Router();
+
+// @router POST api/resume
+// @desc create a resume
+// @access PRIVATE
+router.post('/', auth, async (req, res) => {
+  const {
+    about,
+    experience,
+    carrerGoal,
+    name,
+    jobTitle,
+    addTitle,
+    phone,
+    email,
+    location
+  } = req.body;
+  const createdBy = req.user._id;
+  const data = {
+    createdBy,
+    personalProfile: { about, experience, carrerGoal },
+    details: { name, jobTitle, addTitle },
+    contact: { phone, email, location }
+  };
+  if (
+    !about ||
+    !experience ||
+    !carrerGoal ||
+    !name ||
+    !jobTitle ||
+    !addTitle ||
+    !phone ||
+    !email ||
+    !location
+  ) {
+    return res.status(404).json({ msg: 'Please fill all fields' });
+  }
+  try {
+    const isResume = await Resume.findOne();
+    if (isResume) {
+      throw new Error();
+    }
+    const resume = new Resume(data);
+    resume.save();
+    res.status(200).json(resume);
+  } catch (error) {
+    return res.status(400).json({ msg: 'Unable to register Resume' });
+  }
+});
+
+// @router GET api/resume
+// @desc get the resume
+// @access PUBLIC
+router.get('/', async (req, res) => {
+  try {
+    const resume = await Resume.findOne();
+    res.status(200).json(resume);
+  } catch (error) {
+    res.status(400).json({ msg: 'error' });
+  }
+});
+
+// @router PATCH api/resume
+// @desc update the resume
+// @access PRIVATE
+router.patch('/', auth, async (req, res) => {
+  const {
+    about,
+    experience,
+    carrerGoal,
+    name,
+    jobTitle,
+    addTitle,
+    phone,
+    email,
+    location
+  } = req.body;
+  const createdBy = req.user._id;
+  const data = {
+    createdBy,
+    personalProfile: { about, experience, carrerGoal },
+    details: { name, jobTitle, addTitle },
+    contact: { phone, email, location }
+  };
+  if (
+    !about ||
+    !experience ||
+    !carrerGoal ||
+    !name ||
+    !jobTitle ||
+    !addTitle ||
+    !phone ||
+    !email ||
+    !location
+  ) {
+    return res.status(404).json({ msg: 'Please fill all fields' });
+  }
+  try {
+    const updatedResume = await Resume.findOneAndUpdate(
+      { createdBy: req.user._id },
+      { $set: data },
+      { new: true }
+    );
+    res.status(200).send(updatedResume);
+  } catch (error) {
+    return res.status(400).json({ msg: 'Unable to Update Resume' });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('./db/mongoose');
 const userRouter = require('./routes/api/user');
 // const logoRouter = require('./routes/api/private/logo');
 // const aboutRouter = require('./routes/api/about');
-// const resumeRouter = require('./routes/api/resume');
+const resumeRouter = require('./routes/api/resume');
 // const blogRouter = require('./routes/api/private/blog');
 
 const app = express();
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 // app.use('/api/logo', logoRouter);
 // app.use('/api/about', aboutRouter);
-// app.use('/api/resume', resumeRouter);
+app.use('/api/resume', resumeRouter);
 // app.use('/api/blog', blogRouter);
 
 app.get('*', (req, res) => {
