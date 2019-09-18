f915c9cdebe26e5df7dc239d7ef96e9875a21363
tundeUsman <tundeusman0@gmail.com>
2019-09-18 12:31:58 +0100

    added admin page

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Admin = () => {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className="admin">
+      <div className="User">
+        <Link to="/admin/user">User</Link>
+      </div>
+      <div className="User">
+        <Link to="/admin/resume">Resume</Link>
+      </div>
+      <div className="User">
+        <Link to="/admin/logo">logo</Link>
+      </div>
+      <div className="User">
+        <Link to="/admin/blog">Blog</Link>
+      </div>
     </div>
   );
 };
