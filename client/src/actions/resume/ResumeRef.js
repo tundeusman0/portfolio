db47f6bef80576340a011a104a03d9d440205c3f
tundeUsman <tundeusman0@gmail.com>
2019-09-26 13:13:36 +0100

    refactored resume reference action

--- client/src/actions/resume/ResumeRef.js
+++ client/src/actions/resume/ResumeRef.js
@@ -1,48 +1,11 @@
-import axios from 'axios';
-import tokenConfig from '../../selectors/tokenConfig';
+import { updateAction, deleteAction } from '../../selectors/utils';
 
 export const postRef = payload => async (dispatch, getState) => {
-  try {
-    const res = await axios.patch(
-      '/api/resume/reference',
-      payload,
-      tokenConfig(getState)
-    );
-    dispatch({ type: 'EDIT_RESUME', payload: res.data });
-    dispatch({ type: 'RESUME_SUCCESS' });
-    dispatch({
-      type: 'POST_SUCCESS_MSG',
-      status: 200,
-      id: 'MSG',
-      msg: 'RESUME UPDATED'
-    });
-  } catch (e) {
-    dispatch({
-      type: 'RESUME_ERROR',
-      status: 400,
-      msg: 'Unable to update Resume',
-      id: 'Resume Fail'
-    });
-  }
+  const api = '/api/resume/reference';
+  await updateAction(payload, api, getState, dispatch);
 };
 
 export const deleteRef = id => async (dispatch, getState) => {
-  try {
-    await axios.delete(`/api/resume/reference/${id}`, tokenConfig(getState));
-    dispatch({ type: 'DELETE_REF', id });
-    dispatch({ type: 'RESUME_SUCCESS' });
-    dispatch({
-      type: 'POST_SUCCESS_MSG',
-      status: 200,
-      id: 'MSG',
-      msg: 'RESUME DELETED'
-    });
-  } catch (e) {
-    dispatch({
-      type: 'RESUME_ERROR',
-      status: 400,
-      msg: 'Unable to delete Resume',
-      id: 'Resume Fail'
-    });
-  }
+  const api = `/api/resume/reference/${id}`;
+  await deleteAction(id, api, getState, dispatch, 'reference');
 };
