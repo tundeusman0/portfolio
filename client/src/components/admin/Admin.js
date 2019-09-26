8975d84967a6dfb6ef6bf88580b0b19d30db3043
tundeUsman <tundeusman0@gmail.com>
2019-09-26 11:25:57 +0100

    added userPage clear error

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -3,31 +3,39 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logOut } from './../../actions/auth';
 
-const Admin = ({ logOut, error }) => {
-  const state = {
-    msg: error.id ? error.msg : null
+class Admin extends React.Component {
+  state = {
+    msg: this.props.error.id ? this.props.error.msg : null
   };
-  return (
-    <div className="admin">
-      {state.msg && (
-        <p style={{ fontSize: '20px', color: 'white' }}>{state.msg}</p>
-      )}
-      <button onClick={() => logOut()}>Log Out</button>
-      <div className="User">
-        <Link to="/admin/user">User</Link>
+  componentDidMount() {
+    this.Interval = setInterval(() => this.setState({ msg: '' }), 4000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.Interval);
+  }
+  render() {
+    return (
+      <div className="admin">
+        {this.state.msg && (
+          <p style={{ fontSize: '20px', color: 'white' }}>{this.state.msg}</p>
+        )}
+        <button onClick={() => this.props.logOut()}>Log Out</button>
+        <div className="User">
+          <Link to="/admin/user">User</Link>
+        </div>
+        <div className="User">
+          <Link to="/admin/resume">Resume</Link>
+        </div>
+        <div className="User">
+          <Link to="/admin/logo">logo</Link>
+        </div>
+        <div className="User">
+          <Link to="/admin/blog">Blog</Link>
+        </div>
       </div>
-      <div className="User">
-        <Link to="/admin/resume">Resume</Link>
-      </div>
-      <div className="User">
-        <Link to="/admin/logo">logo</Link>
-      </div>
-      <div className="User">
-        <Link to="/admin/blog">Blog</Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   error: state.authError
