0b0594217010ab630d33db2a1f43ea89c95ed809
tundeUsman <tundeusman0@gmail.com>
2019-09-26 11:13:10 +0100

    added link to userpage

--- client/src/components/admin/EditSkills.js
+++ client/src/components/admin/EditSkills.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Skills from './Skills';
+import { Link } from 'react-router-dom';
 
 const EditSkills = ({ history, match, skills }) => {
   return (
-    <div>
+    <div className="User">
+      <Link to={'/admin/user'}>Back To User Page</Link>
       <Skills
         formName="Edit Skill"
         id={match.params.id}
