6d8cd960cef2e9057fc7344e60afa4f353f94312
tundeUsman <tundeusman0@gmail.com>
2019-09-26 13:18:52 +0100

    refactored resume edit action

--- client/src/actions/resume/resume.js
+++ client/src/actions/resume/resume.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import tokenConfig from '../../selectors/tokenConfig';
+import { updateAction } from '../../selectors/utils';
 
 export const addResume = payload => async (dispatch, getState) => {
   try {
@@ -23,28 +24,8 @@ export const addResume = payload => async (dispatch, getState) => {
 };
 
 export const editResume = payload => async (dispatch, getState) => {
-  try {
-    const res = await axios.patch(
-      '/api/resume',
-      payload,
-      tokenConfig(getState)
-    );
-    dispatch({ type: 'EDIT_RESUME', payload: res.data });
-    dispatch({ type: 'RESUME_SUCCESS' });
-    dispatch({
-      type: 'POST_SUCCESS_MSG',
-      status: 200,
-      id: 'MSG',
-      msg: 'RESUME UPDATED'
-    });
-  } catch (e) {
-    dispatch({
-      type: 'RESUME_ERROR',
-      status: 400,
-      msg: 'Unable to update Resume',
-      id: 'Resume Fail'
-    });
-  }
+  const api = '/api/resume';
+  await updateAction(payload, api, getState, dispatch);
 };
 
 export const postImage = (file = {}) => async (dispatch, getState) => {
