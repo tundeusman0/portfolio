733086c8f4fc243cc569c370bff31144be62646d
tundeUsman <tundeusman0@gmail.com>
2019-10-10 13:41:41 +0100

    added chat component

---
+++ client/src/components/chat/Chat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from '../ContactForm';
+
+const Chat = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'red', textAlign: 'center' }}>Coming Soon</h1>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default Chat;
